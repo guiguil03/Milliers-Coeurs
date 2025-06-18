63d82f2edc1017e1f8353e572c3d1f0d46615774
Ubergizmo <ubergizmogaming@gmail.com>
2025-06-18 23:21:53 +0200

    prod build

--- app.config.js
+++ app.config.js
@@ -1,9 +1,12 @@
-const path = require('path');
-const dotenv = require('dotenv');
+const path = require("path");
+const dotenv = require("dotenv");
 
 // Charger les variables d'environnement à partir du fichier .env (si disponible)
-const env = process.env.NODE_ENV || 'development';
-const envPath = path.resolve(__dirname, env === 'production' ? '.env' : '.env.local');
+const env = process.env.NODE_ENV || "development";
+const envPath = path.resolve(
+  __dirname,
+  env === "production" ? ".env" : ".env.local"
+);
 dotenv.config({ path: envPath });
 
 module.exports = {
@@ -17,40 +20,52 @@ module.exports = {
     splash: {
       image: "./assets/images/splash-icon.png",
       resizeMode: "contain",
-      backgroundColor: "#E0485A"
+      backgroundColor: "#E0485A",
     },
-    assetBundlePatterns: [
-      "**/*"
-    ],
+    assetBundlePatterns: ["**/*"],
     ios: {
-      supportsTablet: true
+      supportsTablet: true,
+      bundleIdentifier: "com.guigui.mld",
     },
     android: {
+      package: "com.guigui.mld",
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#E0485A"
-      }
+        backgroundColor: "#E0485A",
+      },
     },
     web: {
-      favicon: "./assets/images/favicon.png"
+      favicon: "./assets/images/favicon.png",
     },
     scheme: "milliersdecoeurs",
     newArchEnabled: true,
+    plugins: ["expo-router", "expo-web-browser"],
     extra: {
       // Variables Firebase
-      firebaseApiKey: process.env.FIREBASE_API_KEY || "AIzaSyDIJyjyh2j9pUzgRhUZLeRlzj23FDHQBiw",
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN || "millecoeurs-ba7a7.firebaseapp.com",
-      firebaseDatabaseURL: process.env.FIREBASE_DATABASE_URL || "https://millecoeurs-ba7a7-default-rtdb.firebaseio.com",
+      firebaseApiKey:
+        process.env.FIREBASE_API_KEY ||
+        "AIzaSyDIJyjyh2j9pUzgRhUZLeRlzj23FDHQBiw",
+      firebaseAuthDomain:
+        process.env.FIREBASE_AUTH_DOMAIN || "millecoeurs-ba7a7.firebaseapp.com",
+      firebaseDatabaseURL:
+        process.env.FIREBASE_DATABASE_URL ||
+        "https://millecoeurs-ba7a7-default-rtdb.firebaseio.com",
       firebaseProjectId: process.env.FIREBASE_PROJECT_ID || "millecoeurs-ba7a7",
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET || "millecoeurs-ba7a7.firebasestorage.app",
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "397224772460",
-      firebaseAppId: process.env.FIREBASE_APP_ID || "1:397224772460:web:b994c9511b12b9329a2949",
-      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID || "G-3BY2NJZWC4",
-      
+      firebaseStorageBucket:
+        process.env.FIREBASE_STORAGE_BUCKET ||
+        "millecoeurs-ba7a7.firebasestorage.app",
+      firebaseMessagingSenderId:
+        process.env.FIREBASE_MESSAGING_SENDER_ID || "397224772460",
+      firebaseAppId:
+        process.env.FIREBASE_APP_ID ||
+        "1:397224772460:web:b994c9511b12b9329a2949",
+      firebaseMeasurementId:
+        process.env.FIREBASE_MEASUREMENT_ID || "G-3BY2NJZWC4",
+
       // Autres variables d'environnement si nécessaire
       eas: {
-        projectId: "your-project-id"
-      }
-    }
-  }
+        projectId: "your-project-id",
+      },
+    },
+  },
 };
